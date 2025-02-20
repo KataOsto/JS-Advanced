46c9018a8d512fa13c9c57c33bcbbeed09eeaae3
kataosto@gmail.com <kataosto@gmail.com>
2025-02-20 07:27:48 +0100

    1 - What is OOP

---
+++ index.js
@@ -0,0 +1,12 @@
+// 1 - What is OOP
+
+// What is Object-oriented Programing?  styl programowania
+
+//Programowanie obiektowe polega na modelowaniu systemu jako zbioru obiektów, 
+// gdzie każdy obiekt reprezentuje pewien konkretny aspekt systemu . 
+// Obiekty zawierają zarówno funkcje (lub metody), jak i dane.
+
+//programing paradigm?
+//JavaScript jest językiem wieloparadygmatycznym, obsługującym programowanie 
+// imperatywne/proceduralne wraz z programowaniem obiektowym (OOP) i programowaniem funkcyjnym . 
+// JavaScript obsługuje programowanie obiektowe z dziedziczeniem prototypowym.
