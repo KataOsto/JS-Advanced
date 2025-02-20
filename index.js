7afcb5b61820f5cdfcc087dfde146f2032cb0545
kataosto@gmail.com <kataosto@gmail.com>
2025-02-20 07:40:00 +0100

    2 - Four Pillars of OOP

--- index.js
+++ index.js
@@ -10,3 +10,14 @@
 //JavaScript jest językiem wieloparadygmatycznym, obsługującym programowanie 
 // imperatywne/proceduralne wraz z programowaniem obiektowym (OOP) i programowaniem funkcyjnym . 
 // JavaScript obsługuje programowanie obiektowe z dziedziczeniem prototypowym.
+
+// 2 - Four Pillars of OOP
+//Cztery filary programowania obiektów:
+
+//Encapsulation - Hermetyzacja - Enkapsulację można zdefiniować jako „ pakowanie danych i funkcji w jeden komponent ”
+//Abstraction - Abstrakcja - polega na ukrywaniu złożonych szczegółów implementacji i pokazywaniu tylko niezbędnych cech obiektu .
+//Inheritance - Dziedziczenie - przekazywania cech z rodzica do dziecka, tak aby nowy fragment kodu mógł ponownie wykorzystać 
+                             // i rozbudować cechy istniejącego
+//Polymorphism - Polimorfizm - kluczowa koncepcja, która zwiększa skalowalność kodu, możliwość ponownego użycia i łatwość utrzymania . 
+                            // Umożliwia programistom tworzenie kodu, który może dostosowywać się do różnych typów danych i obiektów 
+                            // za pośrednictwem wspólnego interfejsu.
