8b983f404c4fb589c0d79281b8cefe3178a538ca
kataosto@gmail.com <kataosto@gmail.com>
2025-02-21 08:10:26 +0100

    Cleanup

---
+++ start.js
@@ -0,0 +1,42 @@
+// 1 - What is OOP
+
+// What is Object-oriented Programing?  styl programowania
+
+//Programowanie obiektowe polega na modelowaniu systemu jako zbioru obiektów, 
+// gdzie każdy obiekt reprezentuje pewien konkretny aspekt systemu . 
+// Obiekty zawierają zarówno funkcje (lub metody), jak i dane.
+
+//programing paradigm?
+//JavaScript jest językiem wieloparadygmatycznym, obsługującym programowanie 
+// imperatywne/proceduralne wraz z programowaniem obiektowym (OOP) i programowaniem funkcyjnym . 
+// JavaScript obsługuje programowanie obiektowe z dziedziczeniem prototypowym.
+
+// 2 - Four Pillars of OOP
+//Cztery filary programowania obiektów:
+
+//Encapsulation - Hermetyzacja - Enkapsulację można zdefiniować jako „ pakowanie danych i funkcji w jeden komponent ”
+//Abstraction - Abstrakcja - polega na ukrywaniu złożonych szczegółów implementacji i pokazywaniu tylko niezbędnych cech obiektu .
+//Inheritance - Dziedziczenie - przekazywania cech z rodzica do dziecka, tak aby nowy fragment kodu mógł ponownie wykorzystać 
+                             // i rozbudować cechy istniejącego
+//Polymorphism - Polimorfizm - kluczowa koncepcja, która zwiększa skalowalność kodu, możliwość ponownego użycia i łatwość utrzymania . 
+                            // Umożliwia programistom tworzenie kodu, który może dostosowywać się do różnych typów danych i obiektów 
+                            // za pośrednictwem wspólnego interfejsu.
+
+
+ // 3 - Setting Up the Development Environment  
+ 
+ // Extensions - Live Server
+
+ console.log('Hello World');
+
+ // 4 - Course Structure
+
+ // Outline
+
+ //Objects - fundation of object oriented programing
+ //Prototypes - 
+ //Prototypical Inheritance - 
+ //ES6 Classes -
+ //Modules
+
+
