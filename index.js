7432f4cc932fb86299736936b8dac931014cd60f
kataosto@gmail.com <kataosto@gmail.com>
2025-02-28 07:51:49 +0100

    // 1 - Inheretance - dziedziczenie

---
+++ index.js
@@ -0,0 +1,11 @@
+// Prototypes   
+
+// 1 - Inheretance - dziedziczenie
+
+// inheretance is one of core concepts of object-oriented programing rhat enables an 
+//object to take on the properties an methods of another object.
+
+//Exemple - 2 obiekty maja dziedziczyć tę samą mtodę która znajduje sie s np.shape (Nazwana jest Base/super/Parant))
+//          obiekty które dziedziczą jej własności np.cicrle (Nazwana jest Derived/Sub/Child)
+
+// We have to type of Inharatance - Classical and Prototypical
