d75e18f981a53e2a5c85c909f9edd91129c917ec
kataosto@gmail.com <kataosto@gmail.com>
2025-03-05 07:51:23 +0100

    // 2 - Reseting the Constructor

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Prototypical Inheritance
 
 // 1 - Creating Your Own Prototypical Inheritance
-
+/*
 // Prototype inheritance in javascript is the linking of prototypes of a parent object to a child object
 //  to share and utilize the properties of a parent class using a child class.
 
@@ -18,6 +18,34 @@ function Circle(radius) {
 
 Circle.prototype = Object.create(Shape.prototype);
 
+Circle.prototype.draw = function() {
+    console.log('draw');
+}
+
+const s = new Shape();
+const x = new Circle(1);
+*/
+
+
+// 2 - Reseting the Constructor
+
+
+// When you reset the prototype, as a best practice, make sure to reset the constructor as well.
+function Shape() {
+}
+
+Shape.prototype.duplicated = function() {
+    console.log('duplicated');
+}
+
+function Circle(radius) {
+    this.radius = radius;
+}
+
+// Circle.prototype.constructor = Circle;
+// new Circle.prototype.constructor() => new Circle();
+Circle.prototype = Object.create(Shape.prototype);
+Circle.prototype.constructor = Circle;
 Circle.prototype.draw = function() {
     console.log('draw');
 }
