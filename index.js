210d4e9272d74382a8f8cf5cc98af0000d281146
kataosto@gmail.com <kataosto@gmail.com>
2025-03-20 12:02:18 +0100

    // 2 - CommonJS Modules

--- index.js
+++ index.js
@@ -7,21 +7,21 @@
 
 // Benefits of Modulariy- Maintainability, Reuse, Abstract
 
-const _radius = new WeakMap();
+// const _radius = new WeakMap();
 
-class Circle {
-    constructor(radius) {
-        _radius.set(this, radius);
-    }
+// class Circle {
+//     constructor(radius) {
+//         _radius.set(this, radius);
+//     }
 
-    draw() {
-        console.log('Circle with radius' + _radius.get(this));
-    }
-}
+//     draw() {
+//         console.log('Circle with radius' + _radius.get(this));
+//     }
+// }
 
-const c = new Circle(10);
-console.log_(radius.get(c));  // It will return the value of the radius property
-c.draw();
+// const c = new Circle(10);
+// console.log_(radius.get(c));  // It will return the value of the radius property
+// c.draw();
 
 
 // ES5 - nie posiadał modułów, używano zatem:
@@ -30,3 +30,18 @@ c.draw();
 // UMD - Browser aplication and Node.js
 
 // ES6 Modules
+
+
+
+// 2 - CommonJS Modules
+
+// Thing that are highly related should go together
+
+// Cohesion - Spójnosć / W programowaniu komputerowym spójność odnosi się do stopnia, 
+// w jakim elementy wewnątrz modułu są ze sobą powiązane
+
+const Circle = require('./circle');
+
+const c = new Circle(10);
+c.draw();
+
